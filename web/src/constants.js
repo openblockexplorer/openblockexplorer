4d549c2124e27725ba0de199d092f6b0f0a72d6d
Dylan Miller <dylan.christopher.miller@gmail.com>
2018-10-10 03:51:21 -0700

    Connect to back end on Heroku

--- web/src/constants.js
+++ web/src/constants.js
@@ -5,9 +5,12 @@
  */
 
 const Constants = Object.freeze({
-  // Server URIs have to change for production!!!
-  URI_SERVER_HTTP: 'http://localhost:4000',
-  URI_SERVER_WEB_SOCKETS: 'ws://localhost:4000',
+  // Development server URIs
+  //URI_SERVER_HTTP: 'http://localhost:4000',
+  //URI_SERVER_WEB_SOCKETS: 'ws://localhost:4000',
+  // Production server URIs
+  URI_SERVER_HTTP: 'https://dfinityexplorer-server.herokuapp.com/',
+  URI_SERVER_WEB_SOCKETS: 'ws://dfinityexplorer-server.herokuapp.com/',
   URI_CDN_FONT_AWESOME:
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
   URI_CDN_GOOGLE_FONTS: 'https://fonts.googleapis.com/css?family=Istok+Web|Nunito+Sans',
