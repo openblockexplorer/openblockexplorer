7678314fe09c1f53bb12bf8df46d37d02e0db73c
Dylan Miller <dylan.christopher.miller@gmail.com>
2020-07-08 11:44:19 -0700

    Updates

---
+++ precache-manifest.19f30fa236c634a9e3cec49c23fd6e86.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e687336a707995473d98a87be60b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a77388dde149d6b336a",
+    "url": "/static/css/2.436ad326.chunk.css"
+  },
+  {
+    "revision": "0a77388dde149d6b336a",
+    "url": "/static/js/2.be4048a3.chunk.js"
+  },
+  {
+    "revision": "8e40ed79183cc4e6290f",
+    "url": "/static/js/main.6875b9f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ff872a8ae8a94dc0e396313ca81c019f",
+    "url": "/static/media/apollo-logo-dark-mode.ff872a8a.png"
+  },
+  {
+    "revision": "d4f0ec1bd7da2cfdef57a2ff5efd89e1",
+    "url": "/static/media/apollo-logo-light-mode.d4f0ec1b.png"
+  },
+  {
+    "revision": "78a73653f1b74f303de85a68676e3cab",
+    "url": "/static/media/banner-x-horizon.78a73653.png"
+  },
+  {
+    "revision": "c833384ace51db5624971824f271ed5a",
+    "url": "/static/media/github-octocat.c833384a.png"
+  },
+  {
+    "revision": "8d2ae7fdd7bab1e7cb980cf442818a5a",
+    "url": "/static/media/graphql-logo.8d2ae7fd.png"
+  },
+  {
+    "revision": "1d6a085f4e94039b6b8cf6ee8dfb18d0",
+    "url": "/static/media/material-ui-logo.1d6a085f.png"
+  },
+  {
+    "revision": "8135feff91fac2771ba82cf6f9588c14",
+    "url": "/static/media/mysql-logo.8135feff.png"
+  },
+  {
+    "revision": "a32c7cc90e6aec4a8121cab1d03c2b98",
+    "url": "/static/media/nomics-logo-dark-mode.a32c7cc9.png"
+  },
+  {
+    "revision": "6f5390eeb65a6a84fe2134f9bdd1526c",
+    "url": "/static/media/nomics-logo-light-mode.6f5390ee.png"
+  },
+  {
+    "revision": "b8da814f6d2f5883cd63461517b0d25a",
+    "url": "/static/media/open-block-explorer-logo-large.b8da814f.png"
+  },
+  {
+    "revision": "2b07dcdffb3ae60d1f5e7122912e1868",
+    "url": "/static/media/open-block-explorer-logo.2b07dcdf.png"
+  },
+  {
+    "revision": "590aa2e2d41dd0be758dc03f8074342f",
+    "url": "/static/media/prisma-logo-dark-mode.590aa2e2.png"
+  },
+  {
+    "revision": "9d9ad809dc3bf715bbf4040fea1ab512",
+    "url": "/static/media/prisma-logo-light-mode.9d9ad809.png"
+  },
+  {
+    "revision": "24432c0674afb095a88839f5e7bcc094",
+    "url": "/static/media/react-logo-text.24432c06.png"
+  },
+  {
+    "revision": "3b3add6f1c9c97184cb7d56d3ffb896b",
+    "url": "/static/media/styled-components-logo-dark-mode.3b3add6f.png"
+  },
+  {
+    "revision": "33f922e4e0a1779d295c0d51b88ab869",
+    "url": "/static/media/styled-components-logo-light-mode.33f922e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a07e29c4ce02b8366195193a4e07485.js"
+  "/precache-manifest.19f30fa236c634a9e3cec49c23fd6e86.js"
 );
 
 self.addEventListener('message', (event) => {
